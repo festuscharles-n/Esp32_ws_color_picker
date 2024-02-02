cf9aff9bf3a96f86daa054e4cea58327e35cbc9d
Festus Charles <153321373+festuscharles-n@users.noreply.github.com>
2024-02-02 15:47:25 +0100

    Fix: Update Esp32 code

--- esp32_socketIo/esp32_socketIo.ino
+++ esp32_socketIo/esp32_socketIo.ino
@@ -2,10 +2,12 @@
 #include <WiFiMulti.h>
 #include <WebSocketsClient.h>
 #include <WiFiClientSecure.h>
+#include <Arduino.h>
 #include <ArduinoJson.h>
 #include <SocketIoClient.h>
 
 WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
 SocketIOclient socketIO;
 
 #define USE_SERIAL Serial
@@ -16,7 +18,7 @@ const int bluePin = 14;   // Blue LED pin
 
 //WiFi SSID and password
 const char *SSID = "Infinity";
-const char *password = "password";
+const char *password = "password01";
 
 void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length) {
   switch (type) {
@@ -48,23 +50,17 @@ void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length) 
         String eventName = doc[0];
         USE_SERIAL.printf("[IOc] event name: %s\n", eventName.c_str());
 
-        // Message Includes a ID for a ACK (callback)
-        if (id) {
-          // creat JSON message for Socket.IO (ack)
-          DynamicJsonDocument docOut(1024);
-          JsonArray array = docOut.to<JsonArray>();
+        if (eventName == "esp32-event") {
+          // Access the nested JSON object
+          JsonObject data = doc[1];
 
-          // add payload (parameters) for the ack (callback function)
-          JsonObject param1 = array.createNestedObject();
-          param1["now"] = millis();
+          // Extract RGB values from JSON
+          int red = data["red"];
+          int green = data["green"];
+          int blue = data["blue"];
 
-          // JSON to String (serializion)
-          String output;
-          output += id;
-          serializeJson(docOut, output);
-
-          // Send event
-          socketIO.send(sIOtype_ACK, output);
+          // Control the LED color
+          setColor(red, green, blue);
         }
       }
       break;
@@ -117,7 +113,8 @@ void setup() {
 
   // server address, port and URL
   //socketIO.begin("10.11.100.100", 8880, "/socket.io/?EIO=4");
-  socketIO.beginSSL("esp32wscolorpicker-dev-fhdt.4.us-1.fl0.io", 443, "/socket.io/?EIO=3");
+  //socketIO.beginSSL("esp32wscolorpicker-dev-fhdt.4.us-1.fl0.io", 443, "/socket.io/?EIO=4");
+  socketIO.beginSSL("esp32-socketio.onrender.com", 443, "/socket.io/?EIO=4");
 
   // event handler
   socketIO.onEvent(socketIOEvent);
@@ -126,26 +123,6 @@ void setup() {
 unsigned long messageTimestamp = 0;
 void loop() {
   socketIO.loop();
-
-  // socketIO.onEvent("esp32-event", [&](const char *payload, size_t length) {
-  //   // Handle data received from the Node.js server
-  //   DynamicJsonDocument doc(1024);
-  //   DeserializationError error = deserializeJson(doc, payload, length);
-  //   if (error) {
-  //     USE_SERIAL.print(F("deserializeJson() failed: "));
-  //     USE_SERIAL.println(error.c_str());
-  //     return;
-  //   }
-
-  //   int red = doc["red"];
-  //   int green = doc["green"];
-  //   int blue = doc["blue"];
-
-  //   // Use the RGB values as needed
-  //   analogWrite(redPin, red);
-  //   analogWrite(greenPin, green);
-  //   analogWrite(bluePin, blue);
-  // });
 
   uint64_t now = millis();
 
@@ -174,4 +151,11 @@ void loop() {
     // Print JSON for debugging
     USE_SERIAL.println(output);
   }
+}
+
+void setColor(int red, int green, int blue) {
+  Serial.printf("Setting color: R=%d, G=%d, B=%d\n", red, green, blue);
+  analogWrite(redPin, red);
+  analogWrite(greenPin, green);
+  analogWrite(bluePin, blue);
 }
